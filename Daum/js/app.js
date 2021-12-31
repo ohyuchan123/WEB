8766a199e3b4a8f7581903ec142476ae10a9e66f
ohyuchan123 <oyuchan50@gmail.com>
2021-12-31 23:19:52 +0900

    web practice

---
+++ Daum/js/app.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
+const crypto = require('crypto');
+const FileStore = require('session-file-store')(session);
+const cookieParser = require('cookie-parser');
+
