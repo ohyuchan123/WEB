b9d0b3da8e6f2ca4dd5ba3f6dd173883e4a0b05f
ohyuchan123 <oyuchan50@gmail.com>
2022-01-02 18:56:43 +0900

    web practice

--- Daum/js/app.js
+++ Daum/js/app.js
@@ -8,3 +8,10 @@ const crypto = require('crypto');
 const FileStore = require('session-file-store')(session);
 const cookieParser = require('cookie-parser');
 
+const app = express();
+
+const client = mysql.createConnection({
+    user:'root',
+    password:'ikik8785',
+    database:'pro'
+});
